b76fbf6c7be86efd5bf772f51a6e8d76851a66c4
Diana Kirov <dkirov1@gmail.com>
2023-04-01 18:04:13 -0400

    aesthetics

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-console.log("I logged it");
-var _ = require('lodash');
-console.log(_);
 
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
-	+ ", " 
+	+ ", "
 	+ color2.value 
+	+ ", 
+	+ color3.value 
 	+ ")";
 
 	css.textContent = body.style.background + ";";
@@ -21,3 +21,5 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient)
